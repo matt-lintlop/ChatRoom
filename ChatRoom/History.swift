a9ac9aa9c435e891e639ac6d5248ddf9b9ea3844
Matt Lintlop <matt_lintlop@icloud.com>
2017-12-07 18:26:32 -0800

    Implemented History constructor.

--- ChatRoom/History.swift
+++ ChatRoom/History.swift
@@ -1,6 +1,6 @@
 //
 //  History.swift
-//  MyChat
+//  ChatServer
 //
 //  Created by Matthew Lintlop on 12/7/17.
 //  Copyright © 2017 Matthew Lintlop. All rights reserved.
@@ -9,7 +9,12 @@
 import Foundation
 
 struct History : Codable {
-    let command = "?command?"
+    let command = "command"
     var client_time: Int
     var since: Int
+    
+    init(since: Int) {
+        self.since = since
+        self.client_time = currentTime()
+    }
 }

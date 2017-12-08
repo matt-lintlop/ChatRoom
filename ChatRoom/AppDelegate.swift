cf28647d9a055e96feb95644ed36198fba773eb0
Matt Lintlop <matt_lintlop@icloud.com>
2017-12-08 10:57:28 -0800

    Finalized the UI.

--- ChatRoom/AppDelegate.swift
+++ ChatRoom/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
-        UINavigationBar.appearance().barTintColor = UIColor(red: 0, green: 0, blue: 2177/255, alpha: 1)
+        UINavigationBar.appearance().barTintColor = UIColor(red: 0, green: 0, blue: 237/255, alpha: 1)
         UINavigationBar.appearance().tintColor = UIColor.white
         UINavigationBar.appearance().titleTextAttributes = [NSAttributedStringKey.foregroundColor:UIColor.white]
 
